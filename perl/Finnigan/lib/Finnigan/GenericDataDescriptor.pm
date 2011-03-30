11025b7e526afe03243a3dc9ff8248e2beef4a09
Gene Selkov <selkovjr@gmail.com>
2011-03-30 21:26:21 +0100

    inserted ordinal numbers into the keys in GenericDataDescriptor->definition()

--- perl/Finnigan/lib/Finnigan/GenericDataDescriptor.pm
+++ perl/Finnigan/lib/Finnigan/GenericDataDescriptor.pm
@@ -34,6 +34,11 @@ sub label {
   shift->{data}->{"label"}->{value};
 }
 
+sub ordinal {
+  my ($self, $ord) = @_;
+  return $ord . "|" . $self->{data}->{"label"}->{value};
+}
+
 sub stringify {
   my $self = shift;
   my $type = $self->type;
@@ -43,80 +48,80 @@ sub stringify {
 }
 
 sub definition {
-  my $self = shift;
+  my ($self, $ord) = @_;
 
   my $type = $self->type;
 
   # a gap in the listing or a section title
   if ( $type == 0 ) {
-    return;
+    return $self->ordinal($ord) => undef;
   }
 
   # c char (a signed byte)
   if ( $type == 0x1 ) {
-    return $self->label => ['c', 'Int8'];
+    return $self->ordinal($ord) => ['c', 'Int8'];
   }
 
   # bool (true/false)
   if ( $type == 0x2 ) {
-    return $self->label => ['C', 'UInt8 (true/false)'];
+    return $self->ordinal($ord) => ['C', 'UInt8 (true/false)'];
   }
   
   # yes/no
   if ( $type == 0x3 ) {
-    return $self->label => ['C', 'UInt8 (yes/no)'];
+    return $self->ordinal($ord) => ['C', 'UInt8 (yes/no)'];
   }
 
   # on/off
   if ( $type == 0x4 ) {
-    return $self->label => ['C', 'UInt8 (on/off)'];
+    return $self->ordinal($ord) => ['C', 'UInt8 (on/off)'];
   }
 
   # c unsigned char
   if ( $type == 0x5 ) {
-    return $self->label => ['C', 'UInt8'];
+    return $self->ordinal($ord) => ['C', 'UInt8'];
   }
 
   # c short
   if ( $type == 0x6 ) {
-    return $self->label => ['s', 'Int16'];
+    return $self->ordinal($ord) => ['s', 'Int16'];
   }
 
   # c unsigned short
   if ( $type == 0x7 ) {
-    return $self->label => ['v', 'UInt16'];
+    return $self->ordinal($ord) => ['v', 'UInt16'];
   }
 
   # c long
   if ( $type == 0x8 ) {
-    return $self->label => ['l', 'Int32'];
+    return $self->ordinal($ord) => ['l', 'Int32'];
   }
 
   # c unsigned long
   if ( $type == 0x9 ) {
-    return $self->label => ['V', 'UInt32'];
+    return $self->ordinal($ord) => ['V', 'UInt32'];
   }
 
   # c float
   if ( $type == 0xA ) {
-    return $self->label => ['f', 'Float32'];
+    return $self->ordinal($ord) => ['f', 'Float32'];
   }
 
   # c double
   if ( $type == 0xB ) {
-    return $self->label => ['d', 'Float64'];
+    return $self->ordinal($ord) => ['d', 'Float64'];
   }
 
   # asciiz string
   if ( $type == 0xC ) {
     my $l = $self->length;
-    return $self->label => ['string', "ASCIIZ:$l"];
+    return $self->ordinal($ord) => ['string', "ASCIIZ:$l"];
   }
 
   # wide string, zero-terminated
   if ( $type == 0xD ) {
     my $l = $self->length;
-    return $self->label => ['string', "UTF-16-LE:$l"];
+    return $self->ordinal($ord) => ['string', "UTF-16-LE:$l"];
   }
 
   die "unkown data type at $self->{addr}, " . $self;
