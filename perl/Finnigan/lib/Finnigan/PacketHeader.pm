8f382f5715fd94efa6ee097e43d1e52a3924cd10
Gene Selkov <selkovjr@gmail.com>
2014-11-27 02:34:31 -0600

    updated uf-scan to work with converted profiles

--- perl/Finnigan/lib/Finnigan/PacketHeader.pm
+++ perl/Finnigan/lib/Finnigan/PacketHeader.pm
@@ -9,24 +9,45 @@ use base 'Finnigan::Decoder';
 
 
 sub decode {
-  my ($class, $stream) = @_;
+  my ($class, $stream, $type) = @_;
 
-  my $fields = [
-                "unknown long[1]"         => ['V',      'UInt32'],
-                "profile size"            => ['V',      'UInt32'],
-                "peak list size"          => ['V',      'UInt32'],
-                "layout"                  => ['V',      'UInt32'],
-                "descriptor list size"    => ['V',      'UInt32'],
-                "size of unknown stream"  => ['V',      'UInt32'],
-                "size of triplet stream"  => ['V',      'UInt32'],
-                "unknown long[2]"         => ['V',      'UInt32'],
-                "low mz"                  => ['f<',      'Float32'],
-                "high mz"                 => ['f<',      'Float32'],
-               ];
+  my $fields;
+  if ($type and $type == 14) {
+    $fields = [
+      "data offset"    => ['V',   'UInt32'],
+      "low mz"         => ['f<',  'Float32'],
+      "high mz"        => ['f<',  'Float32'],
+      "bin size"       => ['d<',  'Float64'],
+      "profile size"   => ['V',   'UInt32'],
+      "padding 1"      => ['l<',  'Int32'],
+      "padding 2"      => ['l<',  'Int32'],
+      "padding 3"      => ['l<',  'Int32'],
+      "padding 4"      => ['l<',  'Int32'],
+    ];
+  }
+  else {
+    $fields = [
+      "unknown long[1]"         => ['V',      'UInt32'],
+      "profile size"            => ['V',      'UInt32'],
+      "peak list size"          => ['V',      'UInt32'],
+      "layout"                  => ['V',      'UInt32'],
+      "descriptor list size"    => ['V',      'UInt32'],
+      "size of unknown stream"  => ['V',      'UInt32'],
+      "size of triplet stream"  => ['V',      'UInt32'],
+      "unknown long[2]"         => ['V',      'UInt32'],
+      "low mz"                  => ['f<',     'Float32'],
+      "high mz"                 => ['f<',     'Float32'],
+    ];
+  }
 
   my $self = Finnigan::Decoder->read($stream, $fields);
+  $self->{type} = $type ? $type : 0;
 
   return bless $self, $class;
+}
+
+sub data_offset {
+  shift->{data}->{"data offset"}->{value};
 }
 
 sub profile_size {
@@ -41,6 +62,10 @@ sub layout {
   shift->{data}->{"layout"}->{value};
 }
 
+sub type {
+  shift->{type};
+}
+
 sub descriptor_list_size {
   shift->{data}->{"descriptor list size"}->{value};
 }
@@ -51,6 +76,10 @@ sub size_of_unknown_stream {
 
 sub size_of_triplet_stream {
   shift->{data}->{"size of triplet stream"}->{value};
+}
+
+sub bin_size {
+  shift->{data}->{"bin size"}->{value};
 }
 
 sub low_mz {
@@ -82,6 +111,17 @@ Calling this decoder is a pre-requisite to reading any scan data. It
 reads the data packet layout indicator and the sizes of the data
 streams included in the packet.
 
+There are two versions of scans noted so far. Some have a true header that
+preceds variously rendered scan data (profile, peaks, and other data). But
+there is also a variant where there is a 4-byte zero at the start of a packet,
+followed by an array of abundance data, followed by a trailer that contains
+keys to the M/z values. That trailer and the true header are addressed in the
+same way, through a pointer in scan index.
+
+These two types of headers have little in common, Finnigan::PacketHeader is a
+polymorphic object that switches its representation based on the type
+attribute.
+
 =head2 METHODS
 
 =over 4
@@ -94,9 +134,23 @@ The constructor method
 
 Get the layout indicator. Two values have been sighted so far: 0 and 128
 
+=item type
+
+Get header type. The header of (suspected) type 14 is actually a trailer, but
+it is pointed to by scan index and it points to the scan preceding it.
+
+=item data_offset
+
+Get the offset of is packet's data relative to the start of the data stream.
+This attribute is only defined in translated profiles.
+
 =item profile_size
 
 Get the profile size in 4-byte words
+
+=item bin_size
+
+Get the M/z bin size
 
 =item peak_list_size
 
